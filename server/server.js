5a965d65dbb2fde9c877c586a5e856a6c7edf0c0
archercrews <crewsha@gmail.com>
2017-10-18 16:43:53 -0400

    Added express/reorganized project structure

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+
+//Imports
+var indexRoutes = require('./routes/index')
+
+//Create app
+var app = express();
+
+//View engine
+app.set('view engine', 'html');
+app.engine('html', function(path, callbacks) {
+  fs.readFile(path, 'utf-8', callbacks)
+});
+
+//Middleware
+app.use(express.static(path.join(__dirname, '../client')));
+
+//Routes
+app.use('/', indexRoutes);
+
+//Error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+module.exports = app
