7e5d950b1f98ee66ab4ce7baedd26f468b60e5b4
Harrison <crewsha@gmail.com>
2017-11-01 16:39:17 -0400

    Adding navbar

--- client/components/homePage/navMenu.js
+++ client/components/homePage/navMenu.js
@@ -8,22 +8,22 @@ export default class NavMenu extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-light bg-faded">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <ul className="nav nav-pills">
-                      <li className="nav-item">
-                        <a className="nav-link active" href="#">Active</a>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                  <a className="navbar-brand" href="#">Home</a>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                      <li className="nav-item active">
+                        <a className="nav-link" href="#">Characters<span className="sr-only">(current)</span></a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
+                        <a className="nav-link" href="#">Talents</a>
                       </li>
                     </ul>
+                  </div>
                 </nav>
             </div>
         );
